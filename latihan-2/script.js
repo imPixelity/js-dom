a416e0102e754383a1940e18b7258b9eca97b89b
Dzikry <dzikrynp14@gmail.com>
2025-06-29 21:11:27 +0700

    rolling animation

--- latihan-2/script.js
+++ latihan-2/script.js
@@ -10,22 +10,43 @@ const elementList = [
   { element: "grass", winAgainst: "water", icon: "\u{1F343}" },
 ];
 
+function roll() {
+  const imageList = elementList.map(function (item) {
+    return item.element;
+  });
+
+  let i = 0;
+  const rollInterval = setInterval(() => {
+    compPickImg.setAttribute("src", `img/${imageList[i]}.svg`);
+    i = (i + 1) % 3;
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(rollInterval);
+  }, 1000);
+}
+
 function playGame(playerElement) {
   const compElement =
     elementList[Math.floor(Math.random() * elementList.length)];
-  if (playerElement.element === compElement.element) {
-    result.innerHTML = "Draw!\u{1F91D}";
-  } else if (playerElement.winAgainst === compElement.element) {
-    result.innerHTML = "You won!\u{1F3C6}";
-  } else {
-    result.innerHTML = "Ain't no way you lose vro\u{1F940}";
-  }
 
-  resultBoxComp.innerHTML = `Computer picked: ${
-    compElement.element[0].toUpperCase() + compElement.element.slice(1)
-  }${compElement.icon}`;
-  compPickImg.setAttribute("src", `img/${compElement.element}.svg`);
-  resultBoxComp.classList.add("visible");
+  roll();
+
+  setTimeout(() => {
+    if (playerElement.element === compElement.element) {
+      result.innerHTML = "Draw!\u{1F91D}";
+    } else if (playerElement.winAgainst === compElement.element) {
+      result.innerHTML = "You won!\u{1F3C6}";
+    } else {
+      result.innerHTML = "Ain't no way you lose vro\u{1F940}";
+    }
+
+    resultBoxComp.innerHTML = `Computer picked: ${
+      compElement.element[0].toUpperCase() + compElement.element.slice(1)
+    }${compElement.icon}`;
+    compPickImg.setAttribute("src", `img/${compElement.element}.svg`);
+    resultBoxComp.classList.add("visible");
+  }, 1000);
 }
 
 elementBtn.forEach((btn, i) => {
